808c19344240bbf853f473586981d0dcd1a395d7
sophia22001 <yujii22001@gmail.com>
2024-01-08 20:04:59 +0900

    feature : 봉사기록확인 & 마이페이지 소속 수정

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start/StartPage";
 import MainPage from "./pages/Main/MainPage";
-import MyPage from "./pages/MyPage/MyPage";
-import CheckRecord from "./pages/CheckRecord/CheckRecord";
+import Detail from "./pages/MyPage/RecordDetail/Detail";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CheckRecord />} />
+        <Route path="/" element={<Detail />} />
         <Route path="/" element={<StartPage />} />
         <Route path="/main" element={<MainPage />} />
         
