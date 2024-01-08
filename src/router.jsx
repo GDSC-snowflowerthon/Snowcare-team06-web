d6db2d2ec2c7101b71b9f5a54c02e98ea58a2fc9
sophia22001 <yujii22001@gmail.com>
2024-01-09 00:32:49 +0900

    feat : 봉사기록하기 페이지 화면

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start/StartPage";
 import MainPage from "./pages/Main/MainPage";
-import Detail from "./pages/MyPage/RecordDetail/Detail";
+import Record from "./pages/MyPage/Recording/Record";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Detail />} />
+        <Route path="/" element={<Record />} />
         <Route path="/" element={<StartPage />} />
         <Route path="/main" element={<MainPage />} />
         
