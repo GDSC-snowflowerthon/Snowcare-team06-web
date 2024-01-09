12d079e4682ece7ee365c49402e6caad00d088d1
sophia22001 <yujii22001@gmail.com>
2024-01-09 20:32:07 +0900

    feat : 좋아요한 커뮤니티 게시글 페이지 레이아웃

---
+++ src/pages/MyPage/LikeCommunity/LCommunity.jsx
@@ -0,0 +1,43 @@
+import "./LCommunity.css"
+
+export default function LCommunity(){
+  return(
+    <div id="container">
+      <div className="user">
+        <img id="img" src="./src/assets/icon/프로필.png" alt="사용자"></img>
+        <p>구리구리</p>
+      </div>
+      <div>
+        <img id="simg" src="./public/LCom.png"></img>
+      </div>
+      <div className="box2">
+        <p className="title">제목~~~</p>
+        <div>
+          <p className="content">12/30에 폭설이 내린대요~<br/>다같이 눈 치워봐요!</p>
+        </div>
+        <div>
+          <p className="content">2023.12.28</p>
+        </div>
+        <hr className="line"/>
+
+      </div>
+      <div className="user">
+        <img id="img" src="./src/assets/icon/프로필.png" alt="사용자"></img>
+        <p>구리구리</p>
+      </div>
+      <div>
+        <img id="simg" src="./public/LCom.png"></img>
+      </div>
+      <div className="box2">
+        <p className="title">제목~~~</p>
+        <div>
+          <p className="content">12/30에 폭설이 내린대요~<br/>다같이 눈 치워봐요!</p>
+        </div>
+        <div>
+          <p className="content">2023.12.28</p>
+        </div>
+        <hr className="line"/>
+        
+      </div>
+    </div>
+  );}
--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start/StartPage";
 import MainPage from "./pages/Main/MainPage";
-
+import SignupPage from "./pages/Signup/SignupPage";
+import LCommunity from "./pages/MyPage/LikeCommunity/LCommunity";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LCommunity />} />
         <Route path="/" element={<StartPage />} />
-
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/main" element={<MainPage />} />
    
       </Routes>
