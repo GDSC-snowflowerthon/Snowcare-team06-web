502aca480e82885e912c5b62a3db763df9dcd8f6
sophia22001 <yujii22001@gmail.com>
2024-01-09 19:30:50 +0900

    feat : 수정..

--- src/router.jsx
+++ src/router.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start/StartPage";
 import MainPage from "./pages/Main/MainPage";
-import Like from "./pages/MyPage/LikeList/Like";
 
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-       
-        <Route path="/" element={< Like/>} />
-       
         <Route path="/" element={<StartPage />} />
+
         <Route path="/main" element={<MainPage />} />
+   
       </Routes>
     </BrowserRouter>
   );
