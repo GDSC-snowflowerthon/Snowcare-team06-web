cc39ec4f35ff21aebbf98a0ddb35d8193b170480
sophia22001 <yujii22001@gmail.com>
2024-01-09 22:39:43 +0900

    feat : 봉사 기록 상세 레이아웃 수정

--- src/pages/MyPage/RecordDetail/Detail.jsx
+++ src/pages/MyPage/RecordDetail/Detail.jsx
@@ -3,18 +3,20 @@ import "./Detail.css"
 
 export default function Detail(){
   return(
-    <>
+    <div className="container">
       <div className="box">
-        <p className="title">글 제목~~~~~~~~~~</p>
-        <hr style={{border: 'solid 0.1px #D4D6DD'}}/>
-        <p className="content">어제부터 눈이 많이 내려서풍경은 좋았지만 
-        걷다가 미끌어지면 안되니깐 청소하러 나갔다. 사진은 치우기 전의 사진</p>
-        
+        <div>
+          <p className="title">글 제목!!</p>
+        </div>
+        <hr className="hr-style"/>
+        <div className="content">
+          <p>어제부터 눈이 많이 내려서풍경은 좋았지만 
+            걷다가 미끌어지면 안되니깐 청소하러 나갔다. 사진은 치우기 전의 사진.
+            </p>
+        </div>
         <p className="date">2023.01.12</p>
-        <img id="img" src="./CheckRecord-img.png"></img>
+        <img id="snow-img" src="./CheckRecord-img.png"></img>
       </div>
-      
-      
-    </>
+    </div>
   )
 }
--- src/router.jsx
+++ src/router.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start/StartPage";
 import MainPage from "./pages/Main/MainPage";
 import SignupPage from "./pages/Signup/SignupPage";
-import CheckRecord from "./pages/MyPage/CheckRecord/CheckRecord"
-
+import Detail from "./pages/MyPage/RecordDetail/Detail";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CheckRecord />} />
+        <Route path="/" element={<Detail />} />
         <Route path="/" element={<StartPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/main" element={<MainPage />} />
