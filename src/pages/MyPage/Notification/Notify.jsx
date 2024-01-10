954416524ac7f9d532b549f234c99b9b37adbf87
sophia22001 <yujii22001@gmail.com>
2024-01-10 16:16:36 +0900

    feat : 알림 설정 페이지 레이아웃 수정2

--- src/pages/MyPage/Notification/Notify.jsx
+++ src/pages/MyPage/Notification/Notify.jsx
@@ -6,10 +6,10 @@ import './Notify.css';
 const Notify = () => {
   return (
     <>
-      <div id="container">
+      <div id="container6">
         <h3>알림 설정</h3>
         <hr style={{ border: 'solid 0.1px #D4D6DD' }} />
-        <div className="box">
+        <div className="box6">
           <div className='flex'>
             <p>날씨 알림</p>
             <div className='right-button'>
@@ -21,7 +21,7 @@ const Notify = () => {
           <p style={{ fontSize: '13px', color: '#6c7174' }}>매일 9시에 카톡 메시지 전송</p>
           
         </div>
-        <div className="box">
+        <div className="box6">
         <div className='flex'>
             <p>내 장소 봉사활동 알림</p>
             <div className='right-button'>
