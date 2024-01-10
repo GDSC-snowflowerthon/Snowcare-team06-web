de3f52c1a58c29ca432849a117c8e51b2a36281e
sophia22001 <yujii22001@gmail.com>
2024-01-10 16:17:54 +0900

    feat : 매칭완료 채팅방 목록 레이아웃 수정2

--- src/pages/MyPage/ChatList/ChatList.jsx
+++ src/pages/MyPage/ChatList/ChatList.jsx
@@ -4,43 +4,43 @@ import "./ChatList.css"
 export default function CheckList(){
   return(
     <>
-    <h3>참여중인 채팅방</h3>
-    <div id='container'>
+    <h3 className="title-chat">참여중인 채팅방</h3>
+    <div id="container1">
      
 
-      <div className='box'>
+      <div className='box1'>
         <div className='text-container'>
-          <p className='title'>글 제목~~~~</p>
-          <div className='content'>
+          <p className='title1'>글 제목~~~~</p>
+          <div className='content1'>
             <p>내용~~~~~~~~~~~~~~~</p>
           </div>
         </div>
-        <img className="img" src="./CheckRecord-img.png" alt='눈사진'></img>
+        <img className="img1" src="./CheckRecord-img.png" alt='눈사진'></img>
       </div>
 
-      <div className='box'>
+      <div className='box1'>
         <div className='text-container'>
-          <p className='title'>글 제목~~~~</p>
-          <div className='content'>
+          <p className='title1'>글 제목~~~~</p>
+          <div className='content1'>
             <p>내용~~~~~~~~~~~~~~~</p>
           </div>
         </div>
-        <img className="img" src="./CheckRecord-img.png" alt='눈사진'></img>
+        <img className="img1" src="./CheckRecord-img.png" alt='눈사진'></img>
       </div>
 
-      <div className='box'>
+      <div className='box1'>
         <div className='text-container'>
-          <p className='title'>글 제목~~~~</p>
-          <div className='content'>
+          <p className='title1'>글 제목~~~~</p>
+          <div className='content1'>
             <p>내용~~~~~~~~~~~~~~~</p>
           </div>
         </div>
       </div>
 
-      <div className='box'>
+      <div className='box1'>
         <div className='text-container'>
           <p className='title'>글 제목~~~~</p>
-          <div className='content'>
+          <div className='content1'>
             <p>내용~~~~~~~~~~~~~~~</p>
           </div>
         </div>
