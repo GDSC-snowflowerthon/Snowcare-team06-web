c911bd106555536b9d21bafd253d316c1d93ccb5
sophia22001 <yujii22001@gmail.com>
2024-01-10 16:20:08 +0900

    feat : 좋아요한 커뮤니티 게시글 레이아웃 수정3

--- src/pages/MyPage/LikeCommunity/LCommunity.jsx
+++ src/pages/MyPage/LikeCommunity/LCommunity.jsx
@@ -10,7 +10,7 @@ export default function LCommunity(){
   };
   
   return(
-    <div id="container">
+    <div id="container3">
       <div className="user">
         <img id="img" src="./src/assets/icon/프로필.png" alt="사용자"></img>
         <p>구리구리</p>
@@ -18,13 +18,13 @@ export default function LCommunity(){
       <div>
         <img id="simg" src="./LCom.png"></img>
       </div>
-      <div className="box2">
-        <p className="title">제목~~~</p>
+      <div className="box3">
+        <p className="title3">제목~~~</p>
         <div>
-          <p className="content">12/30에 폭설이 내린대요~<br/>다같이 눈 치워봐요!</p>
+          <p className="content3">12/30에 폭설이 내린대요~<br/>다같이 눈 치워봐요!</p>
         </div>
         <div className='content-container'>
-          <p className="content">2023.12.28</p>
+          <p className="content3">2023.12.28</p>
           <h2>
             <span onClick={handleLikeClick}>❤️</span>
             {like}
@@ -41,13 +41,13 @@ export default function LCommunity(){
       <div>
         <img id="simg" src="./LCom.png"></img>
       </div>
-      <div className="box2">
-        <p className="title">제목~~~</p>
+      <div className="box3">
+        <p className="title3">제목~~~</p>
         <div>
-          <p className="content">12/30에 폭설이 내린대요~<br/>다같이 눈 치워봐요!</p>
+          <p className="content3">12/30에 폭설이 내린대요~<br/>다같이 눈 치워봐요!</p>
         </div>
         <div className='content-container'>
-          <p className="content">2023.12.28</p>
+          <p className="content3">2023.12.28</p>
           <h2>
             <span onClick={handleLikeClick}>❤️</span>
             {like}
