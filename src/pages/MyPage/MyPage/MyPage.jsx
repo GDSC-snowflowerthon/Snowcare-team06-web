9e322c314be2ef7117ee37f3b15f3f0b72f78c90
sophia22001 <yujii22001@gmail.com>
2024-01-10 16:11:01 +0900

    Feat : 마이페이지 레이아웃 수정2

--- src/pages/MyPage/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage/MyPage.jsx
@@ -2,42 +2,45 @@ import "./MyPage.css"
 
 export default function MyPage(){
   return(
+    
     <div className="App">
     
     <div className="profile">
-      <img id="round" src="./src/assets/icon/프로필.png"></img>
+      <img id="round" src="./src/assets/images/profile.png"></img>
     </div>
-    <p style={{marginBottom:'60px',fontSize:'20px', textAlign:'center'}}>사용자님</p>
-    <div className="box">
-      <p1>알림설정</p1>
+    <p className="puser" >사용자님</p>
+    <div className="box5">
+      <p>알림설정</p>
       <h3 className="right-button">
         <img className="button-image" src="./src/assets/icon/화살표.svg" alt="버튼이미지" />
       </h3>
     </div>
-    <div className="box">
-      나의 봉사활동 기록
+    <div className="box5">
+      <p>나의 봉사활동 기록</p>
       <h3 className="right-button">
         <img className="button-image" src="./src/assets/icon/화살표.svg" alt="버튼이미지" />
       </h3>
     </div>
-    <div className="box">
-      매칭 완료된 채팅방<h3 className="right-button">
-        <img className="button-image" src="./src/assets/icon/화살표.svg" alt="버튼이미지" />
-      </h3>
-    </div>
-    <div className="box">
-      좋아요한 매칭글
+    <div className="box5">
+      <p>매칭 완료된 채팅방</p>
       <h3 className="right-button">
         <img className="button-image" src="./src/assets/icon/화살표.svg" alt="버튼이미지" />
       </h3>
     </div>
-    <div className="box">
-      좋아요한 커뮤니티 게시글
+    <div className="box5">
+      <p>좋아요한 매칭글</p>
+      <h3 className="right-button">
+        <img className="button-image" src="./src/assets/icon/화살표.svg" alt="버튼이미지" />
+      </h3>
+    </div>
+    <div className="box5">
+      <p>좋아요한 커뮤니티 게시글</p>
       <h3 className="right-button">
         <img className="button-image" src="./src/assets/icon/화살표.svg" alt="버튼이미지" />
       </h3>
     </div>
     <button className="logout-button">로그아웃</button>
   </div>
+ 
   );
 }
